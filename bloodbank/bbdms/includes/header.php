856a2345117abc16824f1534766beb557d5ea6cd
shruti8825 <65104052+shruti8825@users.noreply.github.com>
2022-07-13 21:49:05 +0530

    Add files via upload

---
+++ bloodbank/bbdms/includes/header.php
@@ -0,0 +1,27 @@
+    <nav class="navbar fixed-top navbar-toggleable-md navbar-inverse bg-inverse">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarExample" aria-controls="navbarExample" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="container">
+            <a class="navbar-brand" href="index.php">BLOOD SAVER NGO</a>
+            <div class="collapse navbar-collapse" id="navbarExample">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="become-donar.php">Become a blood Donar</a>
+                    </li>
+                 
+                     <li class="nav-item">
+                        <a class="nav-link" href="search doner.php">Donate us</a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Contact us</a>
+                    </li>
+    
+    
+                 
+                 
+                </ul>
+            </div>
+        </div>
+    </nav>
---
+++ bloodbank/bbdms/index.php
@@ -0,0 +1,168 @@
+<?php
+error_reporting(0);
+include('includes/config.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>BLOOD SAVER NGO</title>
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="css/modern-business.css" rel="stylesheet">
+    <style>
+    .navbar-toggler {
+        z-index: 1;
+    }
+    
+    @media (max-width: 576px) {
+        nav > .container {
+            width: 100%;
+        }
+    }
+    .carousel-item.active,
+    .carousel-item-next,
+    .carousel-item-prev {
+        display: block;
+    }
+    </style>
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+<?php include('includes/header.php');?>
+<?php include('includes/slider.php');?>
+   
+    <!-- Page Content -->
+    <div class="container">
+        <u>
+
+        <h1 class="my-4">WELCOME TO OUR PORTAL</h1>
+        </u>
+
+        <!-- Marketing Icons Section -->
+        <div class="row">
+            <div class="col-lg-4 mb-4">
+                <div class="card">
+                    <h4 class="card-header">The need for blood</h4>
+                   
+                        <p class="card-text" style="padding-left:2%">Every two seconds someone in the India needs blood. It is essential for surgeries, cancer treatment, chronic illnesses, and traumatic injuries. Whether a patient receives whole blood, red cells, platelets or plasma, this lifesaving care starts with one person making a generous donation </p>
+              </div>
+            </div>
+            <div class="col-lg-4 mb-4">
+                <div class="card">
+                    <h4 class="card-header">Why donate us</h4>
+                   
+                        <p class="card-text" style="padding-left:2%">As everyone knows we are working for "BLOODSAVER NGO" which is known for their work and charity in society.</p>
+                </div>
+            </div>
+            <div class="col-lg-4 mb-4">
+                <div class="card">
+                    <h4 class="card-header">Who you could Help</h4>
+                   
+                        <p class="card-text" style="padding-left:2%">Giving blood is a uniquely powerful act. Effective coordination can dramatically increase its life-saving impact</P>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Portfolio Section -->
+        <h2>Some of the Donar</h2>
+
+        <div class="row">
+                   <?php 
+$status=1;
+$sql = "SELECT * from tblblooddonars where status=:status order by rand() limit 6";
+$query = $dbh -> prepare($sql);
+$query->bindParam(':status',$status,PDO::PARAM_STR);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{ ?>
+
+            <div class="col-lg-4 col-sm-6 portfolio-item">
+                <div class="card h-100">
+                    <a href="#"><img class="card-img-top img-fluid" src="images/blood-donor.png" alt="" ></a>
+                    <div class="card-block">
+                        <h4 class="card-title"><a href="#"><?php echo htmlentities($result->FullName);?></a></h4>
+<p class="card-text"><b>  Gender :</b> <?php echo htmlentities($result->Gender);?></p>
+<p class="card-text"><b>Blood Group :</b> <?php echo htmlentities($result->BloodGroup);?></p>
+
+                    </div>
+                </div>
+            </div>
+
+            <?php }} ?>
+          
+            <div class="col-md-8">
+            <h2>HOW WE WORK</h2>
+                <p>We collect blood from our doners who registerd in our website. We go at thier place as per their availability to collect blood after collecting we donate blood that we collected to govt. bloodbanks and hospitals
+ </p>
+            </div>
+
+
+
+        </div>
+        <!-- /.row -->
+
+        <!-- Features Section -->
+        <div class="row">
+            <div class="col-lg-6">
+                <h2>BLOOD GROUPS</h2>
+          <p>  blood group of any human being will mainly fall in any one of the following groups.</p>
+                <ul>
+                
+                
+<li>A positive or A negative</li>
+<li>B positive or B negative</li>
+<li>O positive or O negative</li>
+<li>AB positive or AB negative.</li>
+                </ul>
+                <p>A healthy diet helps ensure a successful blood donation, and also makes you feel better! Check out the following recommended foods to eat prior to your donation.</p>
+            </div>
+            <div class="col-lg-6">
+                <img class="img-fluid rounded" src="images/blood-donor (1).jpg" alt="">
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Call to Action Section -->
+        <div class="row mb-4">
+            <div class="col-md-8">
+            <h4>UNIVERSAL DONORS AND RECIPIENTS</h4>
+                <p>
+The most common blood type is O, followed by type A.
+
+Type O individuals are often called "universal donors" since their blood can be transfused into persons with any blood type. Those with type AB blood are called "universal recipients" because they can receive blood of any type.</p>
+            </div>
+            <div class="col-md-4">
+                <a class="btn btn-lg btn-secondary btn-block" href="become-donar.php">Become a Donar</a>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+  <?php include('includes/footer.php');?>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/tether/tether.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
